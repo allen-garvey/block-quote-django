a23214c0452236e884e8e4220f4f909b634ee7c8
Allen Garvey <allengarvey@hotmail.com>
2015-11-30 00:57:46 -0500

    create quotes app and database migrations

---
+++ quotes/migrations/0001_initial.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Author',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('author_last', models.CharField(max_length=200, null=True, blank=True)),
+                ('author_first', models.CharField(max_length=200)),
+                ('author_middle', models.CharField(max_length=200, null=True, blank=True)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='QuoteGenre',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=200)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='SourceType',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=200)),
+            ],
+        ),
+    ]
