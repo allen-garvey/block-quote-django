54cde63fce5514a2426e41dbe683bab4c1c3b5b4
Allen Garvey <allengarvey@hotmail.com>
2015-12-02 00:11:46 -0500

    add templates directory to be autoloaded

--- block_quote_2/settings.py
+++ block_quote_2/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'block_quote_2.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
