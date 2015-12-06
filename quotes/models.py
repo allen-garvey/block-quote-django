941cb30e6a617a11e376094e554d3633c900c5ca
Allen Garvey <allengarvey@hotmail.com>
2015-12-06 15:35:39 -0500

    fix problem with indentation being part tabs and part spaces

--- quotes/models.py
+++ quotes/models.py
@@ -6,15 +6,15 @@ class Author(models.Model):
     author_first = models.CharField(max_length=200)
     author_middle = models.CharField(max_length=200, blank=True, null=True)
     def __str__(self):
-    	string_name = self.author_first
-    	if self.author_middle is not None:
-    		middle_name = self.author_middle
-    		if len(middle_name) == 1:
-    			middle_name = middle_name + '.'
-    		string_name = string_name + " " + middle_name
-    	if self.author_last is not None:
-    		string_name = string_name + " " + self.author_last
-		return string_name
+        string_name = self.author_first
+        if self.author_middle is not None:
+            middle_name = self.author_middle
+            if len(middle_name) == 1:
+                middle_name = middle_name + '.'
+            string_name = string_name + " " + middle_name
+        if self.author_last is not None:
+            string_name = string_name + " " + self.author_last
+        return string_name
 
 class SourceType(models.Model):
     name = models.CharField(max_length=200)
