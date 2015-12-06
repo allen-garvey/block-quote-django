11cb258403b56db07da32364c75479105a22a00f
Allen Garvey <allengarvey@hotmail.com>
2015-12-06 16:31:23 -0500

    change model str method to unicode to match database

--- quotes/models.py
+++ quotes/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django.utils.encoding import force_bytes
 
 # Create your models here.
 class Author(models.Model):
     author_last = models.CharField(max_length=200, blank=True, null=True)
     author_first = models.CharField(max_length=200)
     author_middle = models.CharField(max_length=200, blank=True, null=True)
-    def __str__(self):
+    def __unicode__(self):
         string_name = self.author_first
         if self.author_middle is not None:
             middle_name = self.author_middle
@@ -18,12 +19,12 @@ class Author(models.Model):
 
 class SourceType(models.Model):
     name = models.CharField(max_length=200)
-    def __str__(self):              # __unicode__ on Python 2
+    def __unicode__(self):              # __unicode__ on Python 2
         return self.name
 
 class QuoteGenre(models.Model):
     name = models.CharField(max_length=200)
-    def __str__(self):              # __unicode__ on Python 2
+    def __unicode__(self):              # __unicode__ on Python 2
         return self.name
 
 class Source(models.Model):
@@ -33,7 +34,7 @@ class Source(models.Model):
     release_date = models.DateField(blank=True, null=True)
     parent_source = models.ForeignKey('self', blank=True, null=True)
     url = models.CharField(max_length=200, blank=True, null=True)
-    def __str__(self):              # __unicode__ on Python 2
+    def __unicode__(self):              # __unicode__ on Python 2
         return self.title
 
 class Quote(models.Model):
@@ -42,13 +43,13 @@ class Quote(models.Model):
     genre = models.ForeignKey(QuoteGenre)
     author = models.ForeignKey(Author, blank=True, null=True)
     source = models.ForeignKey(Source)
-    def __str__(self):              # __unicode__ on Python 2
+    def __unicode__(self):              # __unicode__ on Python 2
         return self.quote_content
 
 class DailyQuote(models.Model):
     quote = models.ForeignKey(Quote)
     date_used = models.DateTimeField(auto_now_add=True)
-    def __str__(self):              # __unicode__ on Python 2
+    def __unicode__(self):              # __unicode__ on Python 2
         return str(self.quote.id) + ' ' + self.date_used.strftime('%m/%d/%Y %H:%M')
 
 
