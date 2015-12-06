1c96d997d932d34ccacd6195cab4c117fce47fdd
Allen Garvey <allengarvey@hotmail.com>
2015-12-06 14:50:11 -0500

    change database to postgres

--- block_quote_2/settings.py
+++ block_quote_2/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'block_quote_2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'block_quote_2',
+        'USER': 'Allen X',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
